491270cac5805dab7931aad835e79543f96045a8
ThanHtutZaw <zgyi5025@gmail.com>
2022-12-11 20:15:54 +0630

    fix : logo snap

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,15 +13,17 @@ export default function Home() {
   useEffect(() => {
     // const target = e.target as HTMLDivElement;
     const nav = document.getElementsByTagName("nav")[0];
-    const main = document.getElementsByTagName('main')[0]
+    const main = document.getElementsByTagName('main')[0] 
     
     main.addEventListener('scroll' , handleScroll)
     function handleScroll(){
       console.log(main.scrollTop);
       if (main.scrollTop > 60) {
         nav.classList.add(styles.sticky);
+        main.style.scrollSnapType = ''
       } else{
         nav.classList.remove(styles.sticky);
+        main.style.scrollSnapType = 'y mandatory'
       }
       
     }
