9b69805fc1f92c18e5556fdc9fb438f378b65943
thanhtutzaw <zgyi5025@gmail.com>
2023-05-19 15:04:47 +0630

    update : redirect to login when session expire

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
       },
     };
   } catch {
-    // context.res.writeHead(302, { Location: "/login" });
+    context.res.writeHead(302, { Location: "/login" });
     context.res.end();
     return {
       props: {
