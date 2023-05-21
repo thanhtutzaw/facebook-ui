d24240d221c3823b334bc5d46a443eef35422f2d
thanhtutzaw <zgyi5025@gmail.com>
2023-05-21 08:13:34 +0630

    css : fix friends info flex wrap

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -8,28 +8,28 @@ import { createContext } from "react";
 const AppContext = createContext<{ user: User | null }>({ user: null });
 export default AppContext;
 
-export function AuthProvider({ children }: any) {
+export function AppProvider({ children }: any) {
   const auth = getAuth(app);
   const [user, setuser] = useState<User | null>(null);
-  useEffect(() => {
-    onIdTokenChanged(auth, async (user) => {
-      // set token in cookie with nookies
-      if (!user) {
-        nookies.destroy(undefined, "token");
-        setuser(null);
-        return;
-      }
-      const token = await user.getIdToken();
-      if (token) {
-        setuser(user);
-        nookies.set(undefined, "token", token, {
-          maxAge: 30 * 24 * 60 * 60,
-          path: "/",
-          secure: true,
-          // httpOnly: true,
-        });
-      }
-    });
-  }, [auth, user]);
-  return <AppContext.Provider value={{ user }}>{children}</AppContext.Provider>;
+  // useEffect(() => {
+  //   onIdTokenChanged(auth, async (user) => {
+  //     // set token in cookie with nookies
+  //     if (!user) {
+  //       nookies.destroy(undefined, "token");
+  //       setuser(null);
+  //       return;
+  //     }
+  //     const token = await user.getIdToken();
+  //     if (token) {
+  //       setuser(user);
+  //       nookies.set(undefined, "token", token, {
+  //         maxAge: 30 * 24 * 60 * 60,
+  //         path: "/",
+  //         secure: true,
+  //         // httpOnly: true,
+  //       });
+  //     }
+  //   });
+  // }, [auth, user]);
+  // return <AppContext.Provider value={{ user }}>{children}</AppContext.Provider>;
 }
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,59 +1,41 @@
 import { getAuth, onIdTokenChanged } from "firebase/auth";
-import { ReactNode, useEffect, useState } from "react";
-import { app } from "../lib/firebase";
 import nookies from "nookies";
-
-import { createContext } from "react";
-import { User } from "firebase/auth";
+import { createContext, useEffect } from "react";
+import { app } from "../lib/firebase";
 import { Props } from "../types/interfaces";
-import Post from "../components/Post/Post";
-// export const AuthContext = createContext<{ user: User | null }>({ user: null });
+
 export const AuthContext = createContext<Props | null>(null);
-// export const AuthContext = createContext<Props>({
-//   posts:
-//   email: null,
-//   myPost?: Post[],
-// });
 
 export function AuthProvider(props: Props) {
   const { uid, allUsers, posts, email, myPost, indicatorRef } = props;
   const auth = getAuth(app);
-  const [user, setuser] = useState<User | null>(null);
+  // const [user, setuser] = useState<User | null>(null);
   useEffect(() => {
     const unsubscribe = onIdTokenChanged(auth, async (user) => {
       if (!user) {
-        // User is not authenticated
         nookies.destroy(undefined, "token");
-        setuser(null);
+        // setuser(null);
         return;
       }
-
       try {
         const token = await user.getIdToken();
-
-        // Update the user state
-        setuser(user);
-
+        // setuser(user);
         // Store the token in a cookie
         nookies.set(undefined, "token", token, {
           maxAge: 30 * 24 * 60 * 60,
           path: "/",
           secure: true,
         });
-
-        // Handle the login process
-        // This could be a function that performs any necessary actions after the user logs in
-        // handleLogin();
       } catch (error) {
         console.log("Error refreshing ID token:", error);
       }
     });
 
     return () => {
-      // Unsubscribe from the onIdTokenChanged listener when the component unmounts
       unsubscribe();
     };
-  }, []); // Empty dependency array ensures the effect runs only once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <AuthContext.Provider
       value={{ uid, allUsers, posts, email, myPost, indicatorRef }}
