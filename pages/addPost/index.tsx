51c82084154b7272865dddc6818fb8dc49e35ed7
thanhtutzaw <zgyi5025@gmail.com>
2023-05-27 09:00:13 +0630

    exit without saving alert

--- pages/addPost/index.tsx
+++ pages/addPost/index.tsx
@@ -1,15 +1,12 @@
 import { faPhotoFilm } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/router";
-import BackHeader from "../../components/Header/BackHeader";
-import s from "../../styles/Home.module.scss";
-import { addPost } from "../../lib/firestore/post";
-import { useContext, useEffect, useRef, useState } from "react";
-import { AppContext } from "../../context/AppContext";
-import { Props } from "../../types/interfaces";
 import { getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import BackHeader from "../../components/Header/BackHeader";
 import { app } from "../../lib/firebase";
-import error from "next/error";
+import { addPost } from "../../lib/firestore/post";
+import s from "../../styles/Home.module.scss";
 export default function AddPost() {
   const router = useRouter();
   const textRef = useRef<HTMLDivElement>(null);
@@ -17,25 +14,32 @@ export default function AddPost() {
   useEffect(() => {
     textRef.current?.focus();
   }, []);
-  // const { uid } = useContext(AppContext) as Props;
   const auth = getAuth(app);
   return (
     <div className={s.addPost}>
-      <BackHeader>
+      <BackHeader
+        onClick={() => {
+          textRef.current?.focus();
+          if (textRef.current?.textContent) {
+            alert("exit without saving");
+          } else {
+            router.back();
+          }
+        }}
+      >
+        {/* <h2>{textRef.current?.textContent === ""}</h2> */}
         <h2>Create Post</h2>
-
         <button
           type="submit"
           className={s.submit}
-          onClick={() => {
+          onClick={async () => {
             textRef.current?.focus();
             const uid = auth.currentUser?.uid;
             if (!textRef.current || !textRef.current.textContent || !uid)
               return;
             try {
-              addPost(uid, textRef.current.textContent, visibility);
-              router.push("/");
-              router.replace(router.asPath);
+              await addPost(uid, textRef.current.textContent, visibility);
+              router.replace("/", undefined, { scroll: false });
             } catch (error: any) {
               alert(error.message);
             }
@@ -62,16 +66,17 @@ export default function AddPost() {
         <select
           onChange={(e) => {
             setvisibility(e.target.value);
-            // alert(e.target.value);
           }}
         >
           <option value="public" key="Public">
             Public
           </option>
-          <option disabled value="friends" key="Friends">
+          <option value="friends" key="Friends">
+            {/* <option disabled value="friends" key="Friends"> */}
             Friends
           </option>
-          <option disabled value="onlyme" key="Only Me">
+          <option value="onlyme" key="Only Me">
+            {/* <option disabled value="onlyme" key="Only Me"> */}
             Only Me
           </option>
         </select>
