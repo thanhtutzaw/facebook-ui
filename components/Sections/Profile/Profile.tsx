d64e011ce22031978ea6bb82ce77dd0240c586c2
thanhtutzaw <zgyi5025@gmail.com>
2023-05-27 13:21:12 +0630

    mypost setting

--- components/Sections/Profile/Profile.tsx
+++ components/Sections/Profile/Profile.tsx
@@ -1,12 +1,13 @@
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { faFilter, faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 // import { Props } from "../../../pages/index";
 import { AppContext } from "../../../context/AppContext";
 import { Post as PostType, Props } from "../../../types/interfaces";
 import Post from "../../Post/Post";
 import s from "./Profile.module.scss";
+import { useActive } from "../../../hooks/useActiveTab";
 export default function Profile() {
   // const { myPost } = props;
   // const { email } = import { AppContext } from "../../../context/AppContext"; as Props;
@@ -21,30 +22,31 @@ export default function Profile() {
   //     text: "foo barr222",
   //   },
   // ];
+  const [active, setactive] = useState(false);
+  const { active: tab } = useActive();
+  const infoRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (tab !== "profile") {
+      setactive(false);
+    }
+  }, [tab]);
+
   return (
-    <div className={s.container}>
-      <div className={s.info}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        {/* <img
-          className={s.profile}
-          alt={email || ""}
-          width={120}
-          height={120}
-          src={
-            email === "testuser@gmail.com"
-              ? "https://www.femalefirst.co.uk/image-library/partners/bang/land/1000/t/tom-holland-d0f3d679ae3608f9306690ec51d3a613c90773ef.jpg"
-              : "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-          }
-        /> */}
+    <div
+      style={{
+        transform: active
+          ? `translateY(-${infoRef?.current?.clientHeight}px)`
+          : "translateY(0px)",
+      }}
+      className={s.container}
+    >
+      <div ref={infoRef} className={s.info}>
         <Image
           className={s.profile}
           width={200}
           height={170}
-          // style={{ objectFit: "cover" }}
           style={{ objectFit: "cover", width: "120px", height: "120px" }}
           alt={email || "profile"}
-          // width={95}
-          // height={105}
           src={
             email === "testuser@gmail.com"
               ? "https://www.femalefirst.co.uk/image-library/partners/bang/land/1000/t/tom-holland-d0f3d679ae3608f9306690ec51d3a613c90773ef.jpg"
@@ -60,8 +62,11 @@ export default function Profile() {
       <div className={s.myPost}>
         <h2 className={s.header}>
           <p>My Posts</p>
-          <button className={s.filterBtn}>
+          <button>
             <FontAwesomeIcon color="#0070f3" icon={faFilter} />
+          </button>
+          <button onClick={() => setactive((prev) => !prev)}>
+            <FontAwesomeIcon color="#0070f3" icon={faGear} />
           </button>
         </h2>
         {myPost?.map((post: PostType) => (
