6a469cb12b97d639f128b0a668af7dd237ea7e31
thanhtutzaw <zgyi5025@gmail.com>
2023-05-29 18:35:36 +0630

    test header

--- pages/index.tsx
+++ pages/index.tsx
@@ -198,12 +198,12 @@ export default function Home({
     console.log(active);
     // if (active === "/") window.location.hash = "#home";
   }, [active]);
-  useEffect(() => {
-    if (expired) {
-      router.push("/");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [expired]);
+  // useEffect(() => {
+  //   if (expired) {
+  //     router.push("/");
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [expired]);
 
   // if (!email || expired) return <Welcome email={""} expired={expired!} />;
   if (expired) return <Welcome />;
