dafe77684b4e0a54817471cbe674f0b6d5a4bbae
thanhtutzaw <zgyi5025@gmail.com>
2023-07-09 00:03:04 +0630

    user page : profile name

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -4,19 +4,30 @@ import BackHeader from "../../components/Header/BackHeader";
 import { db } from "../../lib/firebase";
 import { getUserData } from "../../lib/firebaseAdmin";
 import s from "../../styles/Home.module.scss";
+import { UserRecord } from "firebase-admin/lib/auth/user-record";
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const allUsersQuery = collectionGroup(db, `users`);
     const allUsersSnap = await getDocs(allUsersQuery);
-    const allUsers = allUsersSnap.docs.map((doc) => {
-      const data = doc.data();
-      return {
-        id: doc.id,
-        ...data,
-        ...getUserData(doc.id),
-      };
-    });
+    const allUsers = await Promise.all(
+      allUsersSnap.docs.map(async (doc) => {
+        const data = doc.data();
+        const author = (await getUserData(doc.id)) as UserRecord;
+        // const { displayName:string, photoURL:string } = author;
+        return {
+          id: doc.id,
+          ...data,
+          // ...(author ?? null),
+          // displayName: author.displayName!,
+          // photoURL: author.photoURL!,
+          // ...getUserData(doc.id),
+        };
+      })
+    );
+    console.log(allUsers);
     const user = allUsers.find((u) => u.id === context.query.user);
+    // const userRecord = await getUserData(u.id)
+    console.log(user);
     if (!user) {
       return {
         notFound: true,
@@ -40,7 +51,11 @@ export default function Page({ user }: { user: any }) {
   return (
     <div className="user">
       <BackHeader>
-        <h2 className={s.title}>{user.id}</h2>
+        <h2 className={s.title}>
+          {user.profile
+            ? `${user.profile?.firstName} ${user.profile?.lastName}`
+            : "Unknown User"}
+        </h2>
       </BackHeader>
       <div className={s.userContent}>This page currently display testuser</div>
     </div>
