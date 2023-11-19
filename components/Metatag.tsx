aebc54f5c987dbb7667d5f4b54d932595d470b0f
thanhtutzaw <zgyi5025@gmail.com>
2023-11-19 13:39:45 +0630

    update : meta og image

--- components/Metatag.tsx
+++ components/Metatag.tsx
@@ -18,6 +18,23 @@ export default function Metatag({
       <meta name="theme-color" content="#0070f3" />
       <link rel="icon" href="/logo.svg" />
       <link rel="manifest" href="/manifest.json" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://facebook-ui-zee.vercel.app" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/logo.svg" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://facebook-ui-zee.vercel.app"
+      />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="/logo.svg" />
     </Head>
   );
 }
